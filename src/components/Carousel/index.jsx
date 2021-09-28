7bdd396e2e922372fbe4dc504b67757666fd4b33
archiebald777 <archiechekatouski777@gmail.com>
2021-09-28 20:47:32 +0300

    add scss changes and input

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types'
 class Carousel extends Component {
   constructor(props) {
     super(props);
-    this.state = { currentIndex: 0, interval: 1000 };
+    this.state = { currentIndex: 0, interval: 3000 };
     this.intervalId = null;
   }
 
@@ -46,18 +46,23 @@ stopSlideShow = () => {
       <div>
         <section className={styles.slider}>
           <div className={styles.imgWrapper}>
+          
+          <div className={styles.buttonWrapper}>
             <button className={styles.button} onClick={this.clickHandler}>
               Click to change picture
             </button>
             <button className={styles.button} onClick={this.slideShow}> Start Slideshow </button>
             <button className={styles.button} onClick={this.stopSlideShow}> Stop Slideshow </button>
-            <InputCarousel  intervalTimer = {this.intervalTimer} />
+            <InputCarousel className={styles.input}  intervalTimer = {this.intervalTimer} />
+            
+            </div>
             <img
               className={styles.img}
               src={myPicturesCollection[currentIndex].imgPath}
               alt={myPicturesCollection.label}
             />
           </div>
+          
         </section>
       </div>
     );
--- src/components/InputCarousel/index.jsx
+++ src/components/InputCarousel/index.jsx
@@ -1,13 +1,14 @@
 import { Component } from "react";import Carousel from "../Carousel";
 import PropTypes from 'prop-types'
+import styles from "./InputCarousel.module.scss"
 
 class InputCarousel extends Component{
     render(){
       const { intervalTimer } = this.props
         return(
-            <div>
-              <span>Change interval</span>
-                <input onChange={(e) => {intervalTimer(e.target.value)} } type={Number} />
+            <div className={styles.timerInput}>
+              <p>Change interval</p>
+                <input className={styles.input} onChange={(e) => {intervalTimer(e.target.value)} } type={Number} />
             </div>
         )
     }
